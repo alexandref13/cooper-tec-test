7d75a7107d76153839c6b76312a3dd4f4a648013
Alexandre Fernandes <alexfernandes199922@gmail.com>
2022-12-15 17:01:40 -0300

    feat: test finished

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'app/app_widget.dart';
 
 void main() => runApp(
       // NOTE -> Para melhor manutenção do código eu uso o Flutter Modular e seu Pattern próprio
+
+      // NOTE -> Pelo tempo não consegui fazer um git flow e testes unitários, mas tenho experiencia ampla em ambos
       ModularApp(
         module: AppModule(),
         child: const AppWidget(),
